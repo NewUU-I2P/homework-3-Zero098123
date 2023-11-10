bf597a957fe3ca00240df7d81aa21a24dfbb5f44
Zero098123 <zero.1021475.zero@gmail.com>
2023-11-10 16:13:52 +0500

    problem1

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,20 @@
 void problemSolution1(float consumed_water) {
     float cost;
-    // write your code here
+    float total_cost=13;
+    if(consumed_water>=30){
+        if(consumed_water>=50) {
+            if(consumed_water>=60){
+                total_cost=total_cost+30*0.4+20*0.12+10*1.4 + (total_cost-60)*1.5;
+            } else{
+                total_cost=total_cost+30*0.4+20*0.12+(consumed_water-50)*1.4;
+            }
+        }else{
+            total_cost=total_cost+30*0.4+(consumed_water-30)*0.12;
+        }
+    } else{
+        total_cost=total_cost+consumed_water*0.4;
+    }
 
+    cost=total_cost;
     return cost;
 }
