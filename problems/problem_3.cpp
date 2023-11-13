2b5b62a67a8ef371b5e8525e77fab9295126caef
Zero098123 <zero.1021475.zero@gmail.com>
2023-11-13 16:31:28 +0500

    problem_3

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -9,19 +9,19 @@ string problemSolution3(float height, char S) {
 string answer;
 if(S == 'M') {
     if (height < 1.70) {
-        answer = "Short";
+        answer = " Short";
     } else if (height > 1.70 && height < 1.85) {
-        answer =  "Normal";
+        answer =  " Normal";
     } else {
-        answer ="Tall";
+        answer =" Tall";
     }
 } else if(S=='F'){
     if (height < 1.60) {
         answer = " Short";
     } else if (height > 1.60 && height < 1.75) {
-        answer =  "Normal";
+        answer =  " Normal";
     } else {
-        answer ="Tall";
+        answer =" Tall";
     }
 } else{
     cout<<"there is not such a gender";
