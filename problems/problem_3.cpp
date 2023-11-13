8fece51dbf9e950c66f5d8c4e49e3723c5372232
Zero098123 <zero.1021475.zero@gmail.com>
2023-11-13 16:03:37 +0500

    problem3

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,8 +1,32 @@
 #include <string>
+#include <wsman.h>
 
 std::string problemSolution3(float height, char S) {
     // write your code here
 
+char answer;
+if(S=='M') {
+    if (height < 1.70) {
+        answer = 'Short';
+    } else if (height > 1.70 && height < 1.85) {
+        answer =  'Normal';
+    } else {
+        answer ='Tall';
+    }
+} else if(S=='F'){
+    if (height < 1.60) {
+        answer = 'Short';
+    } else if (height > 1.60 && height < 1.75) {
+        answer =  'Normal';
+    } else {
+        answer ='Tall';
+    }
+} else{
+    std::cout<<"there is not such a gender";
+}
+
     // use return to return your result
     // make use of control flow statements
+    return reinterpret_cast<const char *>(answer);
+
 }
--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <sstream>
 
-std::string problemSolution4(const std::string &macAddress) {
+std::string problemSolution4(const std::string & macAddress) {
     // write your code here
 
     // make use of control flow statements
