7871d82350096894232f34c7a5d49a19b71cc5ef
Zero098123 <zero.1021475.zero@gmail.com>
2023-11-13 16:26:22 +0500

    problem3

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -5,21 +5,21 @@ std::string problemSolution3(float height, char S) {
     // write your code here
 
 std::string answer;
-if(S=='M') {
+if(S == 'M') {
     if (height < 1.70) {
-        answer = "Short";
+        answer = " Short";
     } else if (height > 1.70 && height < 1.85) {
-        answer =  "Normal";
+        answer =  " Normal";
     } else {
-        answer ="Tall";
+        answer =" Tall";
     }
 } else if(S=='F'){
     if (height < 1.60) {
-        answer = "Short";
+        answer = " Short";
     } else if (height > 1.60 && height < 1.75) {
-        answer =  "Normal";
+        answer =  " Normal";
     } else {
-        answer ="Tall";
+        answer =" Tall";
     }
 } else{
     std::cout<<"there is not such a gender";
