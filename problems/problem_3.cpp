963c0a8d74c9c59e223fe965e3993f738240d6d1
Name Surname <98762357+ulugbekovich2113@users.noreply.github.com>
2023-11-14 11:07:02 +0500

    problem3.6

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,27 +2,23 @@
 #include <sstream>
 
 std::string problemSolution3(float height, char S) {
-    // write your code here
-
 std::string answer;
 if(S == 'M') {
     if (height < 1.70) {
-        answer = "Short";
-    } else if (height > 1.70 && height < 1.85) {
-        answer =  "Normal";
+        answer ="Short";
+    } else if (height >= 1.70 && height < 1.85) {
+        answer = "Normal";
     } else {
         answer ="Tall";
     }
 } else if(S=='F'){
     if (height < 1.60) {
-        answer = "Short";
-    } else if (height > 1.60 && height < 1.75) {
-        answer =  "Normal";
-    } else {
+        answer ="Short";
+    } else if (height >= 1.60 && height < 1.75) {
+        answer = "Normal";
+    } else  {
         answer ="Tall";
     }
-} else{
-    cout<<"there is not such a gender";
 }
 
     // use return to return your result
